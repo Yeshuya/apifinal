4e4dc73e8c4e40b7caded26ba3131092d094cef9
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-03-19 14:15:29 +0800

    Update home.php

--- home.php
+++ home.php
@@ -8,13 +8,11 @@
     <td><h1>Name: <?php echo $_SESSION['user_first_name'] ?> <?php echo $_SESSION['user_last_name'] ?> </h1></td>
   </tr>
   <tr>
-    <td><h1>Name: <?php echo $_SESSION['user_first_name'] ?> <?php echo $_SESSION['user_last_name'] ?> </h1></td>
-  </tr>
-  <tr>
     <td><h1>Email: <?php echo $_SESSION['user_email_address'] ?></h1></td>
   </tr>
   <tr>
     <td><h1>Gender: <?php echo $_SESSION['user_gender'] ?></h1></td>
   </tr>
+</table>
 <h1><a href="logout-google.php">Logout</a></h1>
 
