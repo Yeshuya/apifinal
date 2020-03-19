1eb968d81e1c295419832975c4046aa75663cf3c
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-03-19 13:24:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,61 +9,51 @@ $navigation = (isset($_GET['navigation']) && $_GET['navigation'] != '') ? $_GET[
 ?>
 
 
-<html>
-	<head> 
-		<title> API INVENTORY </title>
-		<link rel="stylesheet" type="text/css" href="css/styles_index.css">
-	</head>
-	<body>
-		<div class="header">
-			<a href="index.php"><h3> API INVENTORY </h3></a>
-		</div>
-		<div class="topnav_container">
-			<div class="topnav">
-				<div class="dropdown">
-				    <button class="dropbtn">Product
-				    	<i class="fa fa-caret-down"></i>
-				    </button>
-				    <div class="dropdown-content">
-				      <a href="index.php?page=Product">Manage Product</a>
-				      <a href="index.php?page=AddProd">Add Product</a>
-				    </div>
-				</div>
-				<div class="dropdown">
-				    <button class="dropbtn">Category
-				    	<i class="fa fa-caret-down"></i>
-				    </button>
-				    <div class="dropdown-content">
-				      <a href="index.php?page=Category">Category List</a>
-				    </div>
-				</div>
-			</div>
-		</div>
-		<br>
-		<div class="content">
+<html> 
+    <head> <!-- credits: john paul maja :D <3 -->
+      <title>Endterm Exam</title>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+      <link rel="stylesheet" type="text/css" href="css/style.css">
+      <link href="https://fonts.googleapis.com/css?family=Baloo+Chettan|Fira+Sans|Mukta&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Baloo+Chettan+2&display=swap" rel="stylesheet">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+      
+    </head> 
+    <body> <!-- credits: john paul maja :D <3 -->
+      <div class="header">
+      <a href="index.php"><img src="images/logo.png" style="width:300px;height:200px;"></a>
+      </div>
+
+      <div class="navbar">
+        <a href="index.php">Home</a>
+        <a href="index.php?navigation=product">Products</a>
+        <a href="index.php?navigation=categories">Category</a>
+        <a href="index.php?navigation=create">Create</a>
+      </div>
+
+
         <?php //google ------ credits: john paul maja :D <3
         if($login_button == ''){
-          switch($page){
-            case 'Product':
-		require_once 'product_list.php';
-	    break;
-	    case 'Category':
-	 	require_once 'category_list.php';
-	    break;
-	    case 'AddProd':
-		require_once 'addproduct.php';
-	    break;
-	    case 'Details':
-		require_once 'product_details.php';
-	    break;
-	    case 'Update':
-		require_once 'updateproduct.php';
-	    break;
-	    case 'Delete':
-		require_once 'deleteproduct.php';
-	    break;	
-	    default:
-	    break;
+          switch($navigation){
+            case 'product':
+              require_once 'product.php';
+              break;
+            case 'categories':
+              require_once 'categories.php';
+              break;
+            case 'create':
+              require_once 'form_create.php';
+              break;
+            case 'details':
+              require_once 'product-details.php';
+              break;
+            case 'update':
+              require_once 'form_update.php';
+              break;
+            default:
+              require_once 'home.php';
+              break;
           }
         }else{
           echo '<div align="center">'.$login_button . '</div>';
@@ -91,13 +81,13 @@ $navigation = (isset($_GET['navigation']) && $_GET['navigation'] != '') ? $_GET[
                 case 'update':
                   require_once 'form_update.php';
                   break;
-		default:
+                default:
+                  require_once 'home.php';
                   break;
               }
             }
           
         ?>
-    </div>
     </body><!-- credits: john paul maja :D <3 -->
 </html>
 
