4fa91b2e3b33edb3921f05b04ebe49b2ee802992
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-03-19 13:38:52 +0800

    Update product.php

--- product.php
+++ product.php
@@ -1,44 +1,46 @@
 <?php
-//credits: john paul maja :D
-$json = file_get_contents("http://rdapi.herokuapp.com/product/read.php");
+	$json = file_get_contents('http://rdapi.herokuapp.com/product/read.php');
+ 	$data = json_decode($json,true);
 
-$data = json_decode($json,true);
-$list = $data['records'];
- 
-if(isset($_POST['search'])){
-   $search = $_POST['search'];
-   $jsearch = file_get_contents('http://rdapi.herokuapp.com/product/search.php?s='.$search);
-   $res = json_decode($jsearch,true);
+	$search = (isset($_POST['search']) && $_POST['search'] != '') ? $_POST['search'] : '';
 
-   $list = $res['records'];
-   
-}else{
-   $list = $data['records'];
-}
+	if(isset($search)){
+	$prodsearch = file_get_contents('http://rdapi.herokuapp.com/product/search.php?s='.$search);
+	$rec = json_decode($prodsearch,true);
+
+	$list = $rec['records'];
+	
+ }else{
+	$list = $data['records'];
+ }
 ?>
 
-<h1> Product List </h1> <!-- credits: john paul maja :D <3 -->
+<link rel="stylesheet" type="text/css" href="css/styles_product.css">
+
+<h1> Product List </h1>\
 
 <form action="index.php?navigation=product" method="POST">
     <input type="text" name="search" placeholder="Search Product Name">
 	<input type="submit" name="submit" value="Search">
 </form>
 
-<table><!-- credits: john paul maja :D <3 -->
+<table>
     <tr>
-        <th>ID</th> 
-        <th>Product</th>
-        <th>Price</th>
+        <td> <b>Name</b> </td>
+	<td> <b>Price</b> </td>
+	<td> <b>Description</b> </td>
+	<td> <b>Category</b> </td>
     </tr>
 <?php
-foreach($list as $value){
+foreach($list as $result){
     ?>
     <tr>
-        <td><?php echo $value['id'];?></td>
-        <td><a href="index.php?navigation=details&id=<?php echo $value['id'];?>"><?php echo $value['name'];?></a></td>
-        <td><?php echo $value['price'];?></td>
-    </tr> <!-- credits: john paul maja :D <3 -->
+        <td><a href="index.php?navigation=details&id=<?php echo $result['id'];?>"><?php echo $result['name'];?></a></td>
+        <td><?php echo $result['price'];?></td>
+     	<td><?php echo $result['description']; ?> </td>
+      	<td><?php echo $result['category_name'];?> </td>
+    </tr> 
 <?php
 }
     ?>
-</table><!-- credits: john paul maja :D <3 -->
+</table>
