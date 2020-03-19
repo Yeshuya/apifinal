48c019aa102df776940dd54808f197d97fe96e88
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-03-19 13:53:23 +0800

    Update form_create.php

--- form_create.php
+++ form_create.php
@@ -5,22 +5,40 @@
 	$category = $data['records'];
 ?>
 
-<form action="pro_create.php" method="POST">
+<link rel="stylesheet" type="text/css" href="css/styles_products.css">
 
-<h1> Create Product </h1>
-<input  type="text" name="name" placeholder="name"/>
-<input  type="text" name="description" placeholder="description"/>
-<input  type="text" name="price" placeholder="price"/>
-<select name="category">
-<option value="">--Category--</option>
-	<?php
-      foreach($category as $cview){
-    ?>
-		<option value="<?php echo $cview['id']?>"><?php echo $cview['name']?></option>
-    <?php
-      }
-    ?>
-	</select>
-<input type="submit" name="submit" value="submit"/>
-
+<form method="POST" action="pro_create.php">
+	<br>
+	<h1> ADD PRODUCT </h1>
+	<hr style ="width: 25%;">
+	<br>
+	<table style="border: 1px black solid;">
+		<tr>
+			<td><h2>Name:</h2></td>
+			<td><input type="text" name="name"></td>
+		</tr>
+		<tr>
+			<td><h2>Description: </h2></td>
+			<td><input type="text" name="description"></td>
+		</tr>
+		<tr>
+			<td><h2>Price: </h2></td>
+			<td><input type="text" name="price"></td>
+		</tr>
+		<tr>
+			<td><h2>Category: </h2></td>
+			<td><select type="text" name="category">
+					<option value="" selected> ---Category--- </option>
+				<?php
+					foreach($category as $catSelect)
+					{
+				?>
+					<option value="<?php echo $catSelect['id']; ?>"> <?php echo $catSelect['name']; ?> </option>
+				<?php
+					}
+				?>
+			</select></td>
+		</tr>
+	</table>
+	<button class="confirm" type="submit" value="Create" name="Submit"> Submit </button>
 </form>
