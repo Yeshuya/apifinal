77f882ababd0c24925b740304514665ebdce3c32
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-03-19 14:14:19 +0800

    Update home.php

--- home.php
+++ home.php
@@ -1,21 +1,20 @@
 <h1>Welcome User</h1>
 
-
-<!--google credits by m-a-j-a-->
-<div style="background-color: white;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-  margin-bottom: 25px;
-  margin-left: auto;
-  margin-right: auto;
-  width: 50%;">
-  <img src="<?php echo $_SESSION["user_image"] ?>" alt="profile pic" style="width:100%">
-  <div style="text-align: center;
-  padding: 10px 20px;">
-  <h1>Profile Picture</h1><!-- credits: john paul maja :D <3 -->
-  </div><!-- credits: john paul maja :D <3 -->
-</div><!--credits by m-a-j-a-->
-<h1>Name: <?php echo $_SESSION['user_first_name'] ?> <?php echo $_SESSION['user_last_name'] ?> </h1>
-<h1>Email: <?php echo $_SESSION['user_email_address'] ?></h1>
-
-<h1><a href="logout-google.php">Logout</a></h1><!--credits by m-a-j-a-->
+<table>
+  <tr>
+    <th rowspan="3"><img src="<?php echo $_SESSION["user_image"] ?>" alt="profile pic" style="width:100%"></th>
+  </tr>
+  <tr>
+    <td><h1>Name: <?php echo $_SESSION['user_first_name'] ?> <?php echo $_SESSION['user_last_name'] ?> </h1></td>
+  </tr>
+  <tr>
+    <td><h1>Name: <?php echo $_SESSION['user_first_name'] ?> <?php echo $_SESSION['user_last_name'] ?> </h1></td>
+  </tr>
+  <tr>
+    <td><h1>Email: <?php echo $_SESSION['user_email_address'] ?></h1></td>
+  </tr>
+  <tr>
+    <td><h1>Gender: <?php echo $_SESSION['user_gender'] ?></h1></td>
+  </tr>
+<h1><a href="logout-google.php">Logout</a></h1>
 
