b700cdf6fb3cb86b90566a7c7029ab4e900f9382
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-03-19 13:59:32 +0800

    Update form_update.php

--- form_update.php
+++ form_update.php
@@ -4,30 +4,47 @@
 	$data = json_decode($json,true);
 	$details = array('records' => $data);
 	$result = $details['records'];
-	//category
-	$json2 = file_get_contents('http://rdapi.herokuapp.com/category/read.php');
-	$data2 = json_decode($json2,true);
-	$category = $data2['records'];
+	
+	$jsonCat = file_get_contents('http://rdapi.herokuapp.com/category/read.php');
+	$catData = json_decode($jsonCat,true);
+	$category = $catData['records'];
+
 ?>
 
-	<h1> Update Product </h1>
-<form action="pro_update.php?id=<?php echo $id ?>" method="POST">
-	<input type="text" name="name" value="<?php echo $result['name'];?>"/>
-	<input type="text" name="description" value="<?php echo $result['description']; ?>"/>
-	<input type="text" name="price" value="<?php echo $result['price']; ?>"/>
-	<select name="category">
-	<option value="<?php echo $result['category_id'];?>"><?php echo $result['category_name'];?></option>
-		<?php
-		foreach($category as $cview){
-		?>
-			<option value="<?php echo $cview['id']?>"><?php echo $cview['name']?></option>
-		<?php
-		}
-		?>
-		</select><br>
-	<input type="submit" name="submit" value="submit"/>
+<link rel="stylesheet" type="text/css" href="css/styles_products.css">
 
-	
-
+<form method="POST" action="pro_update.php?id=<?php echo $id ?>">
+	<br>
+	<h1> ADD PRODUCT </h1>
+	<hr style ="width: 25%;">
+	<br>
+	<table style="border: 1px black solid;">
+		<tr>
+			<td><h2>Name:</h2></td>
+			<td><input type="text" name="name" value="<?php echo $result['name'];?>"></td>
+		</tr>
+		<tr>
+			<td><h2>Description: </h2></td>
+			<td><input type="text" name="description" value="<?php echo $result['description'];?>"></td>
+		</tr>
+		<tr>
+			<td><h2>Price: </h2></td>
+			<td><input type="text" name="price" value="<?php echo $result['price'];?>"></td>
+		</tr>
+		<tr>
+			<td><h2>Category: </h2></td>
+			<td><select type="text" name="category">
+					<option value="<?php echo $result['category_id'];?>" selected> <?php echo $result['category_name'];?> </option>
+				<?php
+					foreach($category as $catSelect)
+					{
+				?>
+					<option value="<?php echo $catSelect['id']; ?>"> <?php echo $catSelect['name']; ?> </option>
+				<?php
+					}
+				?>
+			</select></td>
+		</tr>
+	</table>
+	<button class="confirm" type="submit" value="Create" name="Submit"> Update </button>
 </form>
-
