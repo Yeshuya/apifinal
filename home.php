f3651d479f78ac2c5a23e2dc2c10456568438938
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-03-19 14:17:36 +0800

    Update home.php

--- home.php
+++ home.php
@@ -1,17 +1,20 @@
 <h1>Welcome User</h1>
 
+<style>
+table, th, td {
+  border: 1px solid black;
+}
+</style>
+
 <table>
   <tr>
     <th rowspan="3"><img src="<?php echo $_SESSION["user_image"] ?>" alt="profile pic" style="width:100%"></th>
   </tr>
   <tr>
-    <td><h1>Name: <?php echo $_SESSION['user_first_name'] ?> <?php echo $_SESSION['user_last_name'] ?> </h1></td>
+    <td>Name: <?php echo $_SESSION['user_first_name'] ?> <?php echo $_SESSION['user_last_name'] ?> </td>
   </tr>
   <tr>
-    <td><h1>Email: <?php echo $_SESSION['user_email_address'] ?></h1></td>
-  </tr>
-  <tr>
-    <td><h1>Gender: <?php echo $_SESSION['user_gender'] ?></h1></td>
+    <td>Email: <?php echo $_SESSION['user_email_address'] ?></td>
   </tr>
 </table>
 <h1><a href="logout-google.php">Logout</a></h1>
